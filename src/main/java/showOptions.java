fc2ddae6fb3f6738a7ffa3ad61b36cc3d889d132
Ben <meowcat@meowcat.site>
2026-02-03 22:43:31 +0000

    add options (INOP) for caching

--- src/main/java/showOptions.java
+++ src/main/java/showOptions.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ * i know this file has a stupid name.
+ * */
 public class showOptions {
     public static void showOptions() {
         initOptionsUI();
@@ -12,12 +14,19 @@ public class showOptions {
         JFrame frame = new JFrame("Options");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLayout(new FlowLayout());
-        JCheckBox c1 = new JCheckBox("Tick this box?"); // TODO: MAKE THIS DO SOMETHING
+        JCheckBox c1 = new JCheckBox("Enable Caching", true);
+        c1.addActionListener(e -> {
+            if(c1.isSelected()) {
+                boolean iWantCaching = true;
+            } else {
+                boolean iWantCaching = false;
+            }
+        });
         frame.add(c1);
         frame.pack();
         frame.setLocationRelativeTo(null); // this centers the window on spawn
         frame.setVisible(true);
-    } 
+    }
 
 
 }
